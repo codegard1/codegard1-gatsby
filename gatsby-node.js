c8d789a8245c82dcf3897de0ef12338550d9444c
Chris Odegard <c.odegard@gmail.com>
2021-05-19 05:10:40 +0000

    add onCreateWebpackConfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -86,3 +86,16 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig();
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true;
+    }
+    actions.replaceWebpackConfig(config);
+  }
+}
