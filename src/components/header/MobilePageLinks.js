5c3dc58626308515578583fcf364a58b256e45fa
Chris Odegard <c.odegard@gmail.com>
2021-08-23 23:06:43 -0400

    add gallery to mobile links

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -2,16 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 
 const MobilePages = () => {
-    return (
-        <div className="mobile-pages-main">
-            <div className="text-center">
-                <p className="d-inline p-4"><Link to="/"><span className="text-dark">Blog Home</span></Link></p>
-                <p className="d-inline p-4"><Link to="/about"><span className="text-dark">About</span></Link></p>
-                <p className="d-inline p-4"><Link to="/archive"><span className="text-dark">Archive</span></Link></p>
-                <p className="d-inline p-4"><Link to="/blackjack"><span className="text-dark">Blackjack</span></Link></p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="mobile-pages-main">
+      <div className="text-center">
+        <p className="d-inline p-3">
+          <Link to="/"><span className="text-dark">Blog Home</span></Link></p>
+        <p className="d-inline p-2">
+          <Link to="/about"><span className="text-dark">About</span></Link></p>
+        <p className="d-inline p-2">
+          <Link to="/archive"><span className="text-dark">Archive</span></Link></p>
+        <p className="d-inline p-2">
+          <Link to="/blackjack"><span className="text-dark">Blackjack</span></Link></p>
+        <p className="d-inline p-3">
+          <Link to="/gallery"><span className="text-dark">Gallery</span></Link>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default MobilePages
